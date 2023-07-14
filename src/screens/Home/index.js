91d162a5ce8580c0dce0e363ee415fd00552feed
samuelfsd <samuel.estudo778@gmail.com>
2023-07-14 08:57:18 -0300

    feat: add form on Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,19 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
-import { styles, eventName, eventDate, input } from "./styles";
+import {
+  styles,
+  eventName,
+  eventDate,
+  form,
+  input,
+  button,
+  buttonText,
+} from "./styles";
 
 export const Home = () => {
+  const handleParticipantAdd = () => {
+    alert("Adicionando participante");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2023</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante?"
-        placeholderTextColor="#6b6b6b"
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante?"
+          placeholderTextColor="#6b6b6b"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
